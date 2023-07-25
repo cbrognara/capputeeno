f67501d6fad3d260d5c499d8001baeeb93576cea
cbrognara <csbrognara@gmail.com>
2023-07-25 16:53:32 -0300

    feat: api query conect

---
+++ capputeeno/src/components/ProductsList.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { useProducts } from "@/hooks/useProducts"
+
+interface ProductsListProps {
+
+}
+
+export function ProductsList(props: ProductsListProps) {
+    const { data } = useProducts();
+    return (
+        <div></div>
+    )
+}
